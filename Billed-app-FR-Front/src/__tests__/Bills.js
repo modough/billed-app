cc1786a57eac158c1c6fbe65323714c3e30ca9f9
mouhamed <mouhamed.mbaye1@outlook.com>
2023-02-18 01:27:22 +0100

    handled bug on bills.js

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -33,6 +33,9 @@ describe('Given I am connected as an employee', () => {
     test('Then bills should be ordered from earliest to latest', () => {
       document.body.innerHTML = BillsUI({ data: bills });
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => new Date(a.innerHTML));
+
+      //const antiChrono = (a, b) => ((a < b) ? 1 : -1);
+
       const antiChrono = (a, b) => new Date(b.date) - new Date(a.date);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
